28329a8d81babc38e33817b41df0b87bf6513024
bitman <s295103@studenti.polito.it>
2023-06-21 14:06:18 +0200

    debugging

--- fedmd.py
+++ fedmd.py
@@ -100,7 +100,7 @@ class Client():
                 y_pred = self.model(x)
 
                 loss = self.criterion(y_pred, y)
-                print(sum_loss, loss.item())
+                print(sum_loss, loss)
                 sum_loss += loss.item()
 
                 loss.backward()
